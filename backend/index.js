ff893c6a53e890a0c217f5470f5a2a685997bbf4
moisescortes <116118337+moisescortes@users.noreply.github.com>
2024-04-24 18:57:30 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = required("express");
+const bodyParser = require('body-parser');
 
 const Port = 5000;
 
 const app = express()
 
-app.listen(Port, () => {
-    console.log(`Servido ${Port}`);
-})
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'organice'
+});
+connection.connect((error) => {
+    if (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        process.exit(1);
+    }
+    console.log('Conectado a la base de datos MySQL');
+});
